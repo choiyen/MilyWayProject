330be01401b047b427eb8292514ac87e9572f3c0
choiyen <yyy1459@naver.com>
2025-03-08 14:45:57 +0900

    QuestionController 작성

--- src/main/java/project/MilkyWay/Controller/NoticeController.java
+++ src/main/java/project/MilkyWay/Controller/NoticeController.java
@@ -1,11 +1,65 @@
 package project.MilkyWay.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import project.MilkyWay.DTO.ResponseDTO;
+import project.MilkyWay.Entity.NoticeDetailEntity;
+import project.MilkyWay.Entity.NoticeEntity;
+import project.MilkyWay.Expection.FindFailedException;
+import project.MilkyWay.Expection.InsertFailedException;
+import project.MilkyWay.Service.NoticeDetailService;
+import project.MilkyWay.Service.NoticeService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/notice")
 public class NoticeController //Notice, Noticedetaill 동시 동작
 {
+    @Autowired
+    NoticeService noticeService;
+
+    @Autowired
+    NoticeDetailService noticeDetailService;
+
+    ResponseDTO responseDTO = new ResponseDTO<>();
+
+    public ResponseEntity<?> Insert(@RequestBody NoticeEntity notice, @RequestBody NoticeDetailEntity noticeDetailEntity)
+    {
+        try
+        {
+            NoticeEntity notice1 = noticeService.InsertNotice(notice);
+            if(notice1 != null)
+            {
+                NoticeDetailEntity noticeDetailEntity1 = noticeDetailService.InsertNoticeDetallMapper(noticeDetailEntity);
+                if(noticeDetailEntity1 != null)
+                {
+                    List<Object> list = new ArrayList<>();
+                    list.add(notice1);
+                    list.add(noticeDetailEntity1);
+                    return ResponseEntity.<ResponseDTO>ok().body(responseDTO.Response("success","후기 데이터 등록에 성공했습니다.", list));
+                }
+                else
+                {
+                   noticeService.DeleteByNoticeId(notice1.getNoticeId());
+                   throw new InsertFailedException("데이터 저장에 실패했습니다.");
+                }
+            }
+            else
+            {
+                throw new InsertFailedException("데이터 저장에 실패했습니다.");
+            }
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().body(responseDTO.Response("error","후기 등록에 실패했습니다."));
+        }
+    }
+
 }
--- src/main/java/project/MilkyWay/Controller/QuestionsController.java
+++ src/main/java/project/MilkyWay/Controller/QuestionsController.java
@@ -2,16 +2,21 @@ package project.MilkyWay.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.MilkyWay.DTO.QuestionsDTO;
 import project.MilkyWay.DTO.ResponseDTO;
 import project.MilkyWay.Entity.QuestionsEntity;
+import project.MilkyWay.Expection.DeleteFailedException;
 import project.MilkyWay.Expection.FindFailedException;
 import project.MilkyWay.Service.QuestionsService;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.MissingResourceException;
 
 @RestController
 @RequestMapping("/question")
@@ -23,6 +28,22 @@ public class QuestionsController //고객 질문을 관리하기 위한 DTO
 
     ResponseDTO responseDTO = new ResponseDTO<>();
 
+    @PostMapping("/Insert")
+    public  ResponseEntity<?> QuestionInsert(@RequestBody QuestionsDTO questionsDTO)
+    {
+        try
+        {
+            QuestionsEntity questionsEntity = ConVertToEntity(questionsDTO);
+            QuestionsEntity questionsEntity1 = questionsService.Insertquestion(questionsEntity);
+            QuestionsDTO questionsDTO1 = ConVertToDTO(questionsEntity1);
+            return ResponseEntity.badRequest().body(responseDTO.Response("success", "질문 등록에 성공하였습니다." , Collections.singletonList(questionsDTO1)));
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().body(responseDTO.Response("error", "질문 등록에 실패하였습니다." ));
+        }
+    }
+    @PostMapping("/Update")
     public ResponseEntity<?> QuestionUpdate(@RequestBody QuestionsDTO newquestionsDTO)
     {
         try
@@ -46,18 +67,71 @@ public class QuestionsController //고객 질문을 관리하기 위한 DTO
         }
 
     }
-    public  ResponseEntity<?> QuestionInsert(@RequestBody QuestionsDTO questionsDTO)
+    @PostMapping("/Delete")
+    public ResponseEntity<?> QuestionDelete(@RequestBody String QuestionId)
     {
         try
         {
-            QuestionsEntity questionsEntity = ConVertToEntity(questionsDTO);
-            QuestionsEntity questionsEntity1 = questionsService.Insertquestion(questionsEntity);
-            QuestionsDTO questionsDTO1 = ConVertToDTO(questionsEntity1);
-            return ResponseEntity.badRequest().body(responseDTO.Response("success", "질문 등록에 성공하였습니다." , Collections.singletonList(questionsDTO1)));
+            boolean bool = questionsService.DeleteByQuestionId(QuestionId);
+            if(bool)
+            {
+                return ResponseEntity.<ResponseDTO>ok().body(responseDTO.Response("success", "질문 데이터 삭제에 성공하였습니다."));
+            }
+            else
+            {
+                throw new DeleteFailedException("삭제할 데이터가 존재하지 않습니다.");
+            }
         }
         catch (Exception e)
         {
-            return ResponseEntity.badRequest().body(responseDTO.Response("error", "질문 등록에 실패하였습니다." ));
+            return ResponseEntity.badRequest().body(responseDTO.Response("error", "질문 삭제에 실패하였습니다." ));
+
+        }
+    }
+    @PostMapping("/Find")
+    public ResponseEntity<?> QuestionFind(@RequestBody String QuestionId)
+    {
+        try
+        {
+            QuestionsEntity questionsEntity = questionsService.SelectQuestion(QuestionId);
+            if(questionsEntity != null)
+            {
+                QuestionsDTO questionsDTO = ConVertToDTO(questionsEntity);
+                return ResponseEntity.<ResponseDTO>ok().body(responseDTO.Response("success", "질문 데이터 찾기에 성공하였습니다.", Collections.singletonList(questionsDTO)));
+            }
+            else
+            {
+                throw new FindFailedException("질문데이터 찾기 도중 알 수 없는 오류가 발생했습니다.");
+            }
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().body(responseDTO.Response("error", "질문 삭제에 실패하였습니다." ));
+        }
+    }
+    @PostMapping("/FindAll")
+    public ResponseEntity<?> QuestionFindAll()
+    {
+        try
+        {
+            List<QuestionsEntity> questionsEntities = questionsService.findAll();
+            List<QuestionsDTO> questionsDTOS = new ArrayList<>();
+            for(QuestionsEntity questionsEntity : questionsEntities)
+            {
+                questionsDTOS.add(ConVertToDTO(questionsEntity));
+            }
+            if(questionsDTOS.isEmpty())
+            {
+                throw new FindFailedException("질문 조회를 시도했으나, 데이터가 비어있습니다.");
+            }
+            else
+            {
+                return ResponseEntity.<ResponseDTO>ok().body(responseDTO.Response("success","질문 데이터 조회에 성공했습니다.",questionsDTOS));
+            }
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().body(responseDTO.Response("error", "질문 삭제에 실패하였습니다." ));
         }
     }
     private QuestionsEntity ConVertToEntity(QuestionsDTO questionsDTO)
