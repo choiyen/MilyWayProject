1c07b3c0ae309daf6e854fa8476cf126742ad9d0
choiyen <yyy1459@naver.com>
2025-03-20 15:54:52 +0900

    ReservationDTO 수정

--- src/main/java/project/MilkyWay/DTO/ReservationDTO.java
+++ src/main/java/project/MilkyWay/DTO/ReservationDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -38,20 +39,19 @@ public class ReservationDTO //고객의 예약을 관리하기 위한 DTO
     @Schema(description = "예약 고객의 이름", example = "홍길성")
     private String name; // 고객의 이름
 
-    @NotBlank(message = "phone cannot be empty")
-    @Size(min = 5, max= 12, message = "phone는 최소 다섯자리 이상 입력해야 함.")
-    @Pattern(regexp = "^[0-9]+$", message = "숫자만 입력 가능합니다.")
-    @Schema(description = "예약 고객의 전화번호", example = "010-1111-1111")
+    @NotBlank(message = "phoneNumber cannot be empty")
+    @Size(min = 5, message = "phoneNumber는 최소 다섯자리 이상 입력해야 함.")
+    @Pattern(regexp = "^[0-9-]*$", message = "전화번호는 숫자, 하이픈만 입력 가능합니다.")
+    @Schema(description = "고객 전화번호", example = "010-1234-1234")
     private String phone; // 전화번호
 
     @NotBlank(message = "Address cannot be empty")
-    @Size(min = 5, max= 10, message = "아이디는 최소 다섯자리 이상 입력해야 함.")
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "대소문자, 한글, 숫자만 입력 가능합니다.")
+    @Size(min = 5, message = "Address는 최소 다섯자리 이상 입력해야 함.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣\\s]*$", message = "주소는 대소문자, 한글, 숫자, 공백만 입력 가능합니다.")
     @Schema(description = "고객 주소", example = "한국시 아주동 한국파크 101동 4121호")
     private String Address; // 주소(암호화 처리 필요)
 
-    @NotBlank(message = "SubissionDate cannot be empty")
-    @Size(min = 5, max= 15, message = "아이디는 최소 다섯자리 이상 입력해야 함.")
     @Schema(description = "예약 날짜", example = "2025-03-14")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate SubissionDate; // 예약 날짜
 }
